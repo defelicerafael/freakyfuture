fc265e660ea6b614a141b301bcfdb412b5031b6d
Rafu Defelice <defelicerafael@gmail.com>
2022-01-04 14:07:23 -0300

    header w y h

--- templates/header.php
+++ templates/header.php
@@ -1,20 +1,20 @@
 <div class="container-fluid g-0">
   <div class="position-relative ">
     <!-- este es el header nuevo-->
-    <img src="img/header/headernew-1.jpg" class="img-fluid d-none d-sm-block w-100 mx-auto">
-    <img src="img/header/header-cel.jpg" class="img-fluid d-block d-sm-none">
+    <img width="864" height="262" src="img/header/headernew-1.jpg" class="img-fluid d-none d-sm-block w-100 mx-auto">
+    <img width="400" height="365" src="img/header/header-cel.jpg" class="img-fluid d-block d-sm-none">
     <div class="iconos-volumen">
-      <img id="img-audio" src="img/header/play_music.png" title="Freaky Future - Discord" alt="Freaky Future - Discord" class="w80px"> 
+      <img width="200" height="200" id="img-audio" src="img/header/play_music.png" title="Freaky Future - Discord" alt="Freaky Future - Discord" class="w80px"> 
     </div>
     <div class="row g-0 iconos-arriba">
       <div class="col px-1">
         <a href="https://discord.gg/vxJB9cs4J9" alt="Freaky Future - Discord" title="Freaky Future - Discord" target="_blank">
-          <img src="img/header/discord.jpg" title="Freaky Future - Discord" alt="Freaky Future - Discord" class="w80px"> 
+          <img width="100" height="76" src="img/header/discord.jpg" title="Freaky Future - Discord" alt="Freaky Future - Discord" class="w80px"> 
         </a>
       </div>
       <div class="col px-1">
         <a href="https://twitter.com/freaky_future" alt="Freaky Future - Twitter" title="Freaky Future - Twitter" target="_blank">
-          <img src="img/header/twitter.jpg" title="Freaky Future - Twitter" alt="Freaky Future - Twitter" class="w80px"> 
+          <img width="100" height="76" src="img/header/twitter.jpg" title="Freaky Future - Twitter" alt="Freaky Future - Twitter" class="w80px"> 
         </a>
       </div>
     </div>
