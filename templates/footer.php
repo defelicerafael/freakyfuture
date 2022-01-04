019be7d486d1b14d4f42e14b6a82e5eb361c6289
Rafu Defelice <defelicerafael@gmail.com>
2022-01-04 14:09:08 -0300

    Update footer.php

--- templates/footer.php
+++ templates/footer.php
@@ -1,7 +1,7 @@
 <div class="container-fluid g-0 pt-md-5 pt-3">
     <div class="row g-0 justify-content-center align-items-center p-md-0 px-2 qr-footer">
         <div class="col-md-3 col-8 p-md-5 p-2">
-            <img src="img/footer/logosolo.png" alt="" title="" class="img-fluid mx-auto d-block">
+            <img width="645" height="645" src="img/footer/logosolo.png" alt="" title="" class="img-fluid mx-auto d-block">
         </div>
         <div class="col-md-8 col-12 p-md-0">
             <div class="text-md-start text-center mx-auto pt-md-2 pb-md-2 pb-5">
